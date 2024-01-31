6e162fd49b13e2ea79a5eb28057ec1a4dcc67c6c
Myeongji An <marvel0855@gmail.com>
2024-01-30 19:07:45 -0600

    ejs in the mix

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5500;
 // set the view engine to ejs
 let path = require('path');
 app.set('view engine', 'ejs');
